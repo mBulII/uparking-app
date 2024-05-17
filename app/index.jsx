72a80bbe94ecca2fafbc53c5e3c0dfcf93cda639
Bull <papipiro2003@gmail.com>
2024-05-16 21:28:20 -0400

    Project initialized and folder structure defined

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "../styles/Home";
+
+export default function homeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
---
+++ styles/Home.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  //main container
+  container: {
+    flex: 1,
+    backgroundColor: "#B0E0E6",
+  },
+});
